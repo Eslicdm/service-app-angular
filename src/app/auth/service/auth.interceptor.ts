2d5e68829bfe677bb83757a07d1f65c542fee850
Esli Rodrigues <eslicdm1@gmail.com>
2025-11-08 19:11:39 -0300

    Enhance `authInterceptor` to handle 401 errors by triggering logout via `AuthService` and ensure error rethrowing for downstream handling.

--- src/app/auth/service/auth.interceptor.ts
+++ src/app/auth/service/auth.interceptor.ts
@@ -1,15 +1,23 @@
-import {HttpInterceptorFn} from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import {inject} from '@angular/core';
 import {OAuthService} from 'angular-oauth2-oidc';
 import {environment} from '../../../environments/environment';
+import { catchError, throwError } from 'rxjs';
+import { AuthService } from './auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const oAuthService = inject(OAuthService);
+  const authService = inject(AuthService);
   const token = oAuthService.getAccessToken();
 
   if (req.url.startsWith(environment.apiUrl) && token) {
     const authReq = req.clone({ setHeaders: {Authorization: `Bearer ${token}`} });
-    return next(authReq);
+    return next(authReq).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) { authService.logout(); }
+        return throwError(() => error);
+      })
+    );
   }
 
   return next(req);
